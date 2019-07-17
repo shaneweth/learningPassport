37b4f2281cdd97014409281e390a4f59d1fa4afb
willow <shaneweth77@gmail.com>
2019-07-17 12:18:19 -0400

    adding Redux boilerplate

--- mern-auth/client/src/components/layout/Landing.js
+++ mern-auth/client/src/components/layout/Landing.js
@@ -15,9 +15,35 @@ class Landing extends Component {
                         <p className="flow-text grey-text text-darken-1">
                             Create a (minimal) full-stack app with user authentication via Passport.js and JWT's and boobs.
                         </p>
+                        <br />
+                        <div className="col s6">
+                            <Link to="/register"
+                                style={{
+                                    width: "140px",
+                                    borderRadius: "3px",
+                                    letterSpacing: "1.5px"
+                                }}
+                                className="btn btn-large waves-effect waves-light hoverable blue accent-3" >
+                                Register
+                            </Link>
+                        </div>
+                        <div className="col s6">
+                            <Link
+                                to="/login"
+                                style={{
+                                    width: "140px",
+                                    borderRadius: "3px",
+                                    letterSpacing: "1.5px"
+                                }}
+                                className="btn btn-large btn-flat waves-effect white black-text" >
+                                Log In
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
         )
     }
 }
+
+export default Landing;
--- mern-auth/client/src/components/layout/Navbar.js
+++ mern-auth/client/src/components/layout/Navbar.js
@@ -18,3 +18,5 @@ class Navbar extends Component {
         )
     }
 }
+
+export default Navbar;
