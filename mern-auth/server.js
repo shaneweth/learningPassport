7b75a7490985b3f6033d9c032ee0ea673d2ae21e
willow <shaneweth77@gmail.com>
2019-07-16 22:37:16 -0400

    finished backend. created folder structure for FE

--- mern-auth/server.js
+++ mern-auth/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
 
 const app = express();
 
@@ -21,6 +24,16 @@ mongoose.connect(db, { useNewUrlParser: true })
 
     .then(() => console.log("MONGO CONNECT!"))
     .catch(err => console.log(err));
+
+// Passport MW
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
+
 
 const port = process.env.PORT || 5000; // process.env.port is for Heroku, later
 
